888c1a78529ebd25c46b506b5c0faec3d4b61b78
Marcos Miklos Terra <marcos.rmt@gmail.com>
2017-10-28 12:37:09 -0200

    refactor: the changes permit use shelf with bookcase and search pages

--- src/pages/Shelf.js
+++ src/pages/Shelf.js
@@ -1,46 +1,39 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css';
-import { Segment, Grid, Card, Image, Dropdown } from 'semantic-ui-react'
+import { Grid, Card, Image, Dropdown } from 'semantic-ui-react'
 
-const Shelf = ({ books=[], shelves=[], shelfTitle = '', updateShelf }) => {
+const Shelf = ({ books=[], shelves=[], shelfTitle = '', updateShelf, checkImage, columns = 5 }) => {
 
   const options = shelves.concat([{key: 'none', text: 'None', value: 'none' }]);
 
   return (
     <div>
-      <Grid.Row style={{ marginTop: '10px', padding: '10px' }}>
-        <Grid.Column>
-          <Segment raised>
-            <h2>{shelfTitle}</h2>
-            <Grid columns={5} doubling centered>
-            {books.map(book =>(
-              <Grid.Column key={book.id}>
-                <Card>
-                <Image src={book.imageLinks.smallThumbnail} />
-                <Card.Content>
-                  <Card.Header>
-                    {book.title}
-                  </Card.Header>
-                  <Card.Description></Card.Description>
-                </Card.Content>
-                <Card.Content extra>
-                    <Dropdown
-                      floating
-                      pointing='top'
-                      text='Move to...'
-                      options={options}
-                      defaultValue={book.shelf}
-                      onChange={(event, data) => updateShelf(event, data, book)}
-                      />
-                </Card.Content>
-              </Card>
-              </Grid.Column>
-            ))}
-          </Grid>
-        </Segment>
-      </Grid.Column>
-      </Grid.Row>
-    </div>
+      <Grid columns={columns} doubling centered>
+      {books.length > 0 && books.map(book =>(
+        <Grid.Column key={book.id}>
+          <Card>
+          <Image src={book.imageLinks.smallThumbnail} />
+          <Card.Content>
+            <Card.Header>
+              {book.title}
+            </Card.Header>
+            <Card.Description></Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+              <Dropdown
+                floating
+                pointing='top'
+                text='Move to...'
+                options={options}
+                defaultValue={book.shelf}
+                onChange={(event, data) => updateShelf(event, data, book)}
+                />
+          </Card.Content>
+        </Card>
+        </Grid.Column>
+      ))}
+      </Grid>
+  </div>
   )
 }
 export default Shelf
