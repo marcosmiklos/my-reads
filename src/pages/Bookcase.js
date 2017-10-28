94b03ab23ee15807fd03b8373e4f73cf52a083b4
Marcos Miklos Terra <marcos.rmt@gmail.com>
2017-10-28 13:00:29 -0200

    refactor: add link to search page

--- src/pages/Bookcase.js
+++ src/pages/Bookcase.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { Segment, Grid } from 'semantic-ui-react'
 import Shelf from './Shelf'
 
 class Bookcase extends Component {
@@ -11,8 +13,21 @@ class Bookcase extends Component {
       <div className='container'>
 
         {shelves.map(s => (
-          <Shelf key={s.key} books={books.filter(b => b.shelf === s.value)} shelves={shelves} shelfTitle={s.text} updateShelf={updateShelf} />
-        )) }
+          <Grid.Row key={s.key} style={{ marginTop: '10px', padding: '10px' }}>
+            <Grid.Column>
+              <Segment raised>
+                <h2>{s.text}</h2>
+                <Shelf books={books.filter(b => b.shelf === s.value)} shelves={shelves} updateShelf={updateShelf} />
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        ))}
+
+        <div className="open-search">
+          <Link to="/search">
+            Add a book
+          </Link> 
+        </div>
 
       </div>
 
