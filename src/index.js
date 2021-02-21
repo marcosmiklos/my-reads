405d3bce0c0ffc1f62791d2372af32dd500102ea
Marcos Miklos <marcos.rmt@gmail.com>
2021-02-21 16:49:27 -0300

    adicionado espacos para teste

--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
+
+
